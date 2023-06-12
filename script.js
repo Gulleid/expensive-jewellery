be98ef9999851410d08a929dc208725bd272a312
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-12 14:35:55 +0300

    Most expensive Jewelary

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Good Luck 💪🏾
+function mostExpensiveItemName(shopping) {
+    let mostExpensive = null;
+    let mostExpensiveName = '';
+  
+    for (let i = 0; i < shopping.length; i++) {
+      if (!mostExpensive || shopping[i].price > mostExpensive) {
+        mostExpensive = shopping[i].price;
+        mostExpensiveName = shopping[i].name;
+      }
+    }
+  
+    return mostExpensiveName;
+  }
+  
+  let list = [
+    { name: 'diamond', price: 980 },
+    { name: 'gold', price: 250 },
+    { name: 'pearl necklace', price: 4650 }
+  ];
+  
+  console.log(mostExpensiveItemName(list)); 
